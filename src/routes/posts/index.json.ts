43d00ee4d58c095f10d5f7332ff05f958e54de43
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-09 15:10:23 +0100

    PB-17 remove logging, format

--- src/routes/posts/index.json.ts
+++ src/routes/posts/index.json.ts
@@ -1,21 +1,17 @@
 import db from '$lib/database';
 
 interface Body {
-	body: {
-		posts: Array<Post>;
-	};
+  body: {
+    posts: Array<Post>;
+  };
 }
 
 export async function get(): Promise<Body> {
-	const posts: Array<Post> = db.prepare('select * from posts').all();
+  const posts: Array<Post> = db.prepare('select * from posts').all();
 
-	posts.forEach((post) => {
-		post.content = JSON.parse(post.content);
-	});
-
-	return {
-		body: {
-			posts
-		}
-	};
+  return {
+    body: {
+      posts,
+    },
+  };
 }
