19f7ab301e260dc75e58b75152ae69e95394c07c
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-13 21:19:15 +0100

    PB-17 restyle post list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,37 +13,24 @@
 </script>
 
 <script lang="ts">
-  export let posts: Array<Array<Post>>;
-
-  const gridPosts = [];
-  let gridRow = [];
-  posts.forEach((post, idx) => {
-    gridRow.push(post);
-    if ((idx + 1) % 3 === 0 || idx === posts.length - 1) {
-      gridPosts.push(gridRow);
-      gridRow = [];
-    }
-  });
-  posts = gridPosts;
+  export let posts: Array<Post>;
 </script>
 
-<h1>Lorem ipsum</h1>
+<h1>Post list</h1>
 
-<div class="postgrid">
-  {#each posts as postRow}
-    <div class="postrow">
-      {#each postRow as post}
-        <div class="post">
-          <a sveltekit:prefetch href={`/posts/${post.id}`}>
-            <h3>{post.title}</h3>
-          </a>
-          <div bind:innerHTML={post.subtitle} contenteditable="false" />
-          <div>{post.date}</div>
-        </div>
-      {/each}
-    </div>
-  {/each}
-</div>
+{#each posts as post}
+  <div class="post">
+    <h2>
+      <a class="title" sveltekit:prefetch href={`/posts/${post.id}`}>
+        {post.title}
+      </a>
+    </h2>
+    <div class="subtitle" bind:innerHTML={post.subtitle} contenteditable="false" />
+    <br />
+    <div class="date">{post.date}</div>
+    <hr />
+  </div>
+{/each}
 
 <style>
   h1 {
@@ -54,30 +41,25 @@
   }
 
   .post {
-    height: 200px;
-    width: 200px;
-    padding: 16px;
-    margin: 8px;
-    border: 1px solid black;
-
     display: flex;
     flex-direction: column;
-    overflow: scroll;
   }
 
-  .postrow {
-    display: flex;
-    justify-content: flex-start;
-    width: fit-content;
+  .post .title {
+    color: red;
   }
 
-  .postgrid {
-    width: 80%;
-    margin: auto;
+  .post .subtitle {
+    font-size: 1.25em;
+  }
 
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
+  .post .date {
+    font-style: oblique;
+  }
+
+  .post hr {
+    width: 100%;
+    border: none;
+    border-top: 1px solid black;
   }
 </style>
