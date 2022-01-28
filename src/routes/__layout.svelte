d6c349703cfc2a5350a6f8cda37b35d023841c2f
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-28 19:13:03 +0100

    PB-21 add layout and theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,65 @@
-<div class="container">
-    <slot />
+<script>
+  import { onMount } from 'svelte';
+  import { LIGHT, storageTheme } from '$lib/utils.js';
+  import { theme } from '$lib/stores/theme_store.js';
+  import Nav from '../components/Nav.svelte';
+  import Header from '../components/Header.svelte';
+
+  export let segment;
+  onMount(() => {
+    if (process.browser) {
+      theme.useLocalStorage();
+      theme.subscribe((newval) => {
+        document.getElementById('body').setAttribute('class', newval);
+      });
+      document
+        .getElementById('body')
+        .setAttribute('class', localStorage.getItem(storageTheme) || LIGHT);
+    }
+  });
+</script>
+
+<div id="app">
+  <Nav {segment} />
+  <div id="content">
+    <Header />
+    <div id="main">
+      <slot />
+    </div>
+  </div>
 </div>
 
 <style>
-    .container {
-        width: 70%;
-        margin: auto;
+  #content {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  #app {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    font-size: larger;
+    height: 100vh;
+  }
+
+  #main {
+    text-align: justify;
+    padding: 30px 10px;
+  }
+
+  @media screen and (min-width: 650px) {
+    #app {
+      flex-direction: row;
+      justify-content: center;
     }
+    #content {
+      display: flex;
+      flex-direction: column;
+      width: 750px;
+      padding-top: 50px;
+    }
+  }
 </style>
