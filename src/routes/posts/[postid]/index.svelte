67faab0c7ed50d11edd5939a47482ee6aa3feab6
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-29 16:56:46 +0100

    PB-21 fix post styling

--- src/routes/posts/[postid]/index.svelte
+++ src/routes/posts/[postid]/index.svelte
@@ -21,6 +21,35 @@
 
 <a class={INLINE_CLASS} href="/" sveltekit:prefetch>Back to main page</a>
 
-<h1>{post.title}</h1>
+<h1 class="post-title">{post.title}</h1>
 
 <div class="post-content" bind:innerHTML={post.content} contenteditable="false" />
+
+<br />
+<hr />
+
+<a class={INLINE_CLASS} href="/" sveltekit:prefetch>Back to main page</a>
+
+<style scoped lang="css">
+  .post-title {
+    padding: 2rem 0;
+  }
+  .post-content :global(h2) {
+    padding: 1.5rem 0;
+  }
+  .post-content :global(h3) {
+    padding: 1rem 0;
+  }
+  .post-content :global(h4) {
+    padding: 0.5rem 0;
+  }
+
+  .post-content :global(ul),
+  .post-content :global(ol) {
+    padding-left: 2rem;
+  }
+
+  .post-content :global(li) {
+    padding: 0.5rem;
+  }
+</style>
