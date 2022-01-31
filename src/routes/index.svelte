ed8f9961f0b005f585a30cf6f029815e6dcad7ce
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-31 22:48:39 +0100

    PB-29 fetch tags separately

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,19 @@
   import type { Load } from '@sveltejs/kit';
 
   export const load: Load = async ({ fetch }) => {
-    const res = await fetch('/posts.json');
+    let res = await fetch('/posts.json');
     const { posts } = await res.json();
+
+    for (const post of posts) {
+      res = await fetch(`/tags/${post.id}.json`);
+      const { tags } = await res.json();
+      post.tags = tags;
+    }
+
     return {
       props: {
         posts,
+        noPosts: !posts.length,
       },
     };
   };
@@ -14,7 +22,8 @@
 
 <script lang="ts">
   import PostListViewGroup from '$lib/components/PostViews/PostListViewGroup.svelte';
-  export let posts: Array<Post>;
+  export let posts: Post[];
+  export let noPosts: boolean;
 </script>
 
-<PostListViewGroup {posts} />
+<PostListViewGroup {posts} {noPosts} />
