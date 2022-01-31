675044832f699a5850d8e66c4c3d661f7d868d13
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-31 22:33:38 +0100

    PB-29 add tag interface

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,15 @@
 /// <reference types="@sveltejs/kit" />
 
 interface Post {
-    id: number;
-    date: string;
-    title: string;
-    subtitle: string;
-    content: string;
+  id: number;
+  date: string;
+  title: string;
+  subtitle: string;
+  content: string;
+  tags: Tag[];
+}
+
+interface Tag {
+  id: number;
+  tag_name: string;
 }
