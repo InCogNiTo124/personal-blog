15597ca773f39f64dec9d7c6ce574eeca6eb2778
amalija-ramljak <amalija.ramljak@gmail.com>
2022-01-31 14:18:42 +0100

    PB-21 style title

--- src/lib/components/PostViews/PostFullView.svelte
+++ src/lib/components/PostViews/PostFullView.svelte
@@ -8,7 +8,11 @@
 
   <div class="post-title">
     <h1>{post.title}</h1>
-    <p>{post.date}</p>
+    <p class="subtitle">{post.subtitle}</p>
+    <div class="post-meta">
+      <p>Tags: {post.date}</p>
+      <p>{post.date}</p>
+    </div>
   </div>
 
   <div class="post-content" bind:innerHTML={post.content} contenteditable="false" />
@@ -22,7 +26,25 @@
 <style scoped lang="css">
   .post-title {
     padding: 2rem 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
+
+  .post-title .subtitle {
+    color: var(--caption-grey);
+    font-style: oblique;
+  }
+
+  .post-meta {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    font-size: 0.875rem;
+  }
+
   .post-content :global(h2) {
     padding: 1.5rem 0;
   }
