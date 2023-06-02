2a0bcc7ff0700a487b9f2847296c39d0c19a6686
InCogNiTo124 <msmetko@msmetko.xyz>
2023-06-02 21:45:35 +0200

    Migrate tags endpoint

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,30 +1,42 @@
-#[macro_use] extern crate rocket;
-use rocket::serde::Serialize;
+#[macro_use]
+extern crate rocket;
 use rocket::serde::json::Json;
+use rocket::serde::Serialize;
 use sqlite;
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
+struct Tag {
+    id: u32,
+    tag_name: String,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
 struct TagList {
-    tags: Vec<String>
+    tags: Vec<Tag>,
 }
 
 #[get("/posts/<post_id>/tags")]
 fn post_tags(post_id: u32) -> Json<TagList> {
     let connection = sqlite::open("/db.sqlite3").unwrap();
-    let query = "SELECT tag_name FROM tags;";
-    let mut tags: Vec<String> = vec![];
+    let query = "select tags.tag_name as tag_name, tags.id as id from post_tags join tags on post_tags.tag_id = tags.id where post_tags.post_id = ?;";
+    let mut tags: Vec<Tag> = vec![];
     // TODO optimize
-    connection.iterate(query, |pairs| {
-        for &(name, value) in pairs.iter() {
-            match (name) {
-                "tag_name" => tags.push(String::from(value.unwrap())),
-                _ => {}
-            }
-        }
-        true
-    }).unwrap();
-    Json(TagList { tags: tags})
+    for row in connection
+        .prepare(query)
+        .unwrap()
+        .into_iter()
+        .bind((1, post_id as i64))
+        .unwrap()
+        .map(|row| row.unwrap())
+    {
+        tags.push(Tag {
+            id: row.read::<i64, _>("id") as u32,
+            tag_name: row.read::<&str, _>("tag_name").to_string(),
+        });
+    }
+    Json(TagList { tags: tags })
 }
 
 #[launch]
--- src/routes/api/getters/tags/[postid=integer]/+server.ts
+++ src/routes/api/getters/tags/[postid=integer]/+server.ts
@@ -1,4 +1,3 @@
-import db from '$lib/server/database';
 import { json } from '@sveltejs/kit';
 
 interface Body {
@@ -6,22 +5,34 @@ interface Body {
 }
 
 /** @type {import('./$types').RequestHandler} */
-export function GET({ params }): Response {
+// export async function GET({ params }): Promise<Response> {
+//   const { postid } = params;
+//   // page = 1+
+
+//   // const responseData: Body = {
+//     // posts: posts.length === 11 ? posts.slice(0, -1) : posts,
+//     // lastPage: posts.length < 11,
+//   // };
+//   
+//   return json(responseData);
+// }
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ params }): Promise<Response> {
   const { postid } = params;
 
-  const tags = db
-    .prepare(
-      `select tags.tag_name, tags.id
-              from post_tags
-              join tags
-              on post_tags.tag_id = tags.id
-              where post_tags.post_id = ?`
-    )
-    .all(postid);
+  // const tags = db
+  //   .prepare(
+  //     `select tags.tag_name, tags.id
+  //             from post_tags
+  //             join tags
+  //             on post_tags.tag_id = tags.id
+  //             where post_tags.post_id = ?`
+  //   )
+  //   .all(postid);
 
-  const responseData: Body = {
-    tags,
-  };
+  const responseData: Body = await fetch(`http://blog-db/posts/${postid}/tags`).then(res => res.json());
+  // console.log(responseData);
 
   return json(responseData);
 }
